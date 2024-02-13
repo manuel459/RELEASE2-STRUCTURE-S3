7bc428aff240d1cff095fb2a3a597b53fb4d061c
GaboAlfaro20 <gabriel.alfaro@materiagris.pe>
2024-02-13 17:21:09 -0500

    REASEGUROS R.NEGOCIO TRAZA

---
+++ code/Glue/Reaseguros/fndtifrs17gluereasegurod02_test.py
@@ -0,0 +1,145 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+import boto3
+import io
+import json
+import importlib.util
+from boto3.dynamodb.conditions import Key
+
+#INICIALIZAR OBJETOS
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+s3_client = boto3.client('s3')
+glue_client = boto3.client('glue')
+s3r = boto3.resource('s3')
+cliente_dynamodb = boto3.client("dynamodb")
+ssm = boto3.client('ssm', 'us-east-1')
+id = 'REASEGURO'
+nombre_error = '-'
+job_name = 'fndtifrs17gluereasegurod02_test'
+response = glue_client.get_job_runs(JobName=job_name, MaxResults=1)
+
+#FUNCIÓN PARA EJECUTAR UN SCRIPT GUARDADO EN UN BUCKET S3
+def execute_script(name_bucket, name_object):
+    path_temp = 'fileTemp.py'
+    s3r.Object(name_bucket, name_object).download_file(path_temp)
+    #importando
+    spec = importlib.util.spec_from_file_location('module', path_temp)
+    structure = importlib.util.module_from_spec(spec)
+    #lee mi script
+    spec.loader.exec_module(structure)
+    return structure
+    
+#EXTRACCION DE LAS CONFIGURACIONES Y RETORNARLAS EN UN DICCIONARIO DE DATOS
+def extract_config(l_configuraciones, nombre_tabla):
+    #CREAR UN DICCIONARIO PARA ESTABLECER LAS CONFIGURACIONES
+    l_dic_config = {}
+    
+    #Extraer las CONFIGURACIONES NECESARIAS segun la lista
+    for dominio in l_configuraciones:
+        config = cliente_dynamodb.get_item(TableName=nombre_tabla, Key={'NOMBRE_DOMINIO': {'S': str(dominio['DOMINIO'])}})
+        l_dic_config[config['Item']['NOMBRE_DOMINIO']['S']] = json.loads(config['Item'][dominio['COLUMNA']]['S'])
+       
+    return l_dic_config
+
+last_run = response['JobRuns'][0]
+print("ID-GLUE")
+print(last_run)
+
+last_start_time = last_run['StartedOn']
+
+print(f"Last Start Time: {last_start_time}")
+
+#EXTRACCION DE LAS CONFIGURACIONES PARA LOS AMBIENTES 
+def get_ssm():
+        parameter_name = "/configuracion/variable/entorno"
+        response = ssm.get_parameter(
+                Name=parameter_name,
+                WithDecryption=True  # Descifra el valor si es un SecureString
+            )
+        return response['Parameter']['Value']
+
+try:
+    #-------------------------------------#
+    #   EXTRAER VARIABLES DE ENTORNO
+    #-------------------------------------#
+    env = get_ssm()
+    
+    #-------------------------------------#
+    #   EXTRAER CONFIGURACIONES DYNAMODB
+    #-------------------------------------#
+    
+    #CREAR UNA LISTA CON TODAS LAS CONFIGURACIONES NECESARIAS SEGUN EL DOMINIO
+    l_configuraciones = [{ "DOMINIO": "GENERAL" , "COLUMNA": "ESTRUCTURA" }, { "DOMINIO": "REASEGUROS" , "COLUMNA": "NEGOCIO" }]
+    
+    #NOMBRE DE LA TABLA DE CONFIGURACIONES
+    
+    nombre_tabla = f'TablaTestIFRS17'
+    
+    #CREAR UN DICCIONARIO PARA ESTABLECER LAS CONFIGURACIONES
+    l_dic_config = {}
+
+    #EXTRAER CONFIGURACIONES
+    l_dic_config = extract_config(l_configuraciones, nombre_tabla)
+    print(l_dic_config)
+    
+    #--------------------------------------#
+    #  CONTROL DE EJECUCION DYNAMODB
+    #--------------------------------------#
+    
+    #EXTRAR LA FUNCION DE LA TRAZABILIDAD 
+    trazabilidad = execute_script(l_dic_config['GENERAL']['bucket']['artifact'],l_dic_config['GENERAL']['funciones']['log'])
+    
+    #EXTRAR EL TIPO DE CARGA DEL DYNAMODB
+    tipo_carga = l_dic_config['GENERAL']['tipoCarga']
+    
+    #EJECUTAR LA TRAXABILIDAD
+    trazabilidad.update_log(cliente_dynamodb, id, 1,1, nombre_error,last_start_time,tipo_carga)
+    
+    #--------------------------------------------------#
+    #    EJECUCIÓN DEL GRUPO DE INFORMACIÓN PRODUCTOS
+    #--------------------------------------------------#
+    
+    for config in l_dic_config['REASEGUROS']['path_file_tmp']:
+        if config['flag'] == 1:
+            
+            print(config['script'])
+            print(config['tablas'])
+            
+            #OBTENER SCRIPTS ALMACENADOS EN S3
+            structure = execute_script(l_dic_config['GENERAL']['bucket']['artifact'], config['script'])
+
+            #LLAMAR Y LANZAR LOS PARAMETROS A LA FUNCION getData
+            L_DF_REASEGUROS = structure.get_data(glueContext, l_dic_config['GENERAL']['bucket']['artifact'] ,config['tablas'],l_dic_config['GENERAL']['fechas']['dFecha_Inicio'] , l_dic_config['GENERAL']['fechas']['dFecha_Fin'])
+            
+            print(L_DF_REASEGUROS)
+        
+            if L_DF_REASEGUROS is not None:
+                #Trasformar a bit escrito en formato txt
+                L_BUFFER_REASEGUROS = io.BytesIO()
+                L_DF_REASEGUROS.toPandas().to_parquet(L_BUFFER_REASEGUROS, index=False)
+                L_BUFFER_REASEGUROS.seek(0)
+                
+                # Escribir el objeto Parquet en S3
+                s3_client.put_object(
+                    Bucket = l_dic_config['GENERAL']['bucket']['artifact'],
+                    Key = config['path'],
+                    Body=L_BUFFER_REASEGUROS.read()
+                    )
+    
+    #EJECUTAR LA TRAZABILIDAD CAPTURANDO LOS TIEMPOS
+    trazabilidad.update_log(cliente_dynamodb, id, 2,1,nombre_error, last_start_time,tipo_carga)
+
+except Exception as e:
+    # Log the error for debugging purposes
+    print(f"Error: {str(e)}")
+    nombre_error = str(e)
+    trazabilidad.update_log(cliente_dynamodb, id, 2,1,nombre_error, last_start_time,tipo_carga)
+
+job.commit()
