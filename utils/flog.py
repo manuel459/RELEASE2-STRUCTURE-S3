e3daef99a062f5ce9162f8a75d7eedd3c79ade60
Manuel <manuel.chirre@materiagris.pe>
2024-02-14 12:06:11 -0500

    ALTERAR LA TABLA DE TRAZA PARA CAPTURAR LOS HISTORICOS EN CAMPOS DE DOMINIO

--- utils/flog.py
+++ utils/flog.py
@@ -20,21 +20,38 @@ def update_log(cliente_dynamodb, id, step,number,nombre_error,last_start_time,ti
     fecha_inicio = last_start_time.astimezone(limaTz).strftime('%Y/%m/%d %I:%M:%S')
 
     if step == 1:
-        cliente_dynamodb.update_item(
-            TableName=table,
-            Key={
-                "DOMINIO": {"S": id}
-            },
-            UpdateExpression="set FECHA_INICIO = :fi, FECHA_FIN = :ff, STEP = :st, TIPO_CARGA = :tc,ERROR_MESSAGE = :ne,TIEMPO = :te,T_G_LECTURA = :tgl",
-            ExpressionAttributeValues={
+        #SI ES LECTURA LIMPIA LOS CAMPOS HISTORICOS
+        if number == 0:
+            l_UpdateExpression = 'set FECHA_INICIO = :fi, FECHA_FIN = :ff, STEP = :st, TIPO_CARGA = :tc,ERROR_MESSAGE = :ne,TIEMPO = :te, T_G_LECTURA = :tgl, T_G_NEGOCIO = :tgn, T_G_ESTRUCTURA = :tge'
+            l_ExpressionAttributeValues = {
                 ':fi': {'S': fecha_inicio},
                 ':ff': {'S': 'ESPERE'},
                 ':st': {'S': steps[number]},
                 ':tc': {'S': tipo_carga},
                 ':ne': {'S': '-'},
                 ':te': {'S': ' '},
-                ':tgl':{'S': ' '}
+                ':tgl':{'S': ' '},
+                ':tgn':{'S': ' '},
+                ':tge':{'S': ' '}
             }
+        else:
+            l_UpdateExpression = 'set FECHA_INICIO = :fi, FECHA_FIN = :ff, STEP = :st, TIPO_CARGA = :tc,ERROR_MESSAGE = :ne,TIEMPO = :te'
+            l_ExpressionAttributeValues = {
+                ':fi': {'S': fecha_inicio},
+                ':ff': {'S': 'ESPERE'},
+                ':st': {'S': steps[number]},
+                ':tc': {'S': tipo_carga},
+                ':ne': {'S': '-'},
+                ':te': {'S': ' '}
+            }       
+        
+        cliente_dynamodb.update_item(
+            TableName=table,
+            Key={
+                "DOMINIO": {"S": id}
+            },
+            UpdateExpression = l_UpdateExpression,
+            ExpressionAttributeValues = l_ExpressionAttributeValues
         )
 
     else:
@@ -45,16 +62,36 @@ def update_log(cliente_dynamodb, id, step,number,nombre_error,last_start_time,ti
         diferencia = fecha_fin_dt - fecha_inicio_dt
         fecha_t_g_l = f"{fecha_fin} - {str(diferencia)}"
 
+        if number == 0:
+            l_UpdateExpression = 'set FECHA_FIN = :ff, ERROR_MESSAGE = :ne, TIEMPO = :te, T_G_LECTURA = :tgl'
+            l_Atributos = {
+                ':ff': {'S': fecha_fin},
+                ':ne': {'S': nombre_error},
+                ':te': {'S': str(diferencia)},
+                ':tgl':{'S': fecha_t_g_l}
+            }
+        elif number == 1:
+            l_UpdateExpression = 'set FECHA_FIN = :ff, ERROR_MESSAGE = :ne, TIEMPO = :te, T_G_NEGOCIO = :tgn'
+            l_Atributos = {
+                ':ff': {'S': fecha_fin},
+                ':ne': {'S': nombre_error},
+                ':te': {'S': str(diferencia)},
+                ':tgn':{'S': fecha_t_g_l}
+            }
+        elif number == 2:
+            l_UpdateExpression = 'set FECHA_FIN = :ff, ERROR_MESSAGE = :ne, TIEMPO = :te, T_G_ESTRUCTURA = :tge'
+            l_Atributos = {
+                ':ff': {'S': fecha_fin},
+                ':ne': {'S': nombre_error},
+                ':te': {'S': str(diferencia)},
+                ':tge':{'S': fecha_t_g_l}
+            }  
+        
         cliente_dynamodb.update_item(
             TableName=table,
             Key={
                 "DOMINIO": {"S": id}
             },
-            UpdateExpression= "set FECHA_FIN = :ff, ERROR_MESSAGE = :ne, TIEMPO = :te,T_G_LECTURA = :tgl",
-            ExpressionAttributeValues= {
-                ':ff': {'S': fecha_fin},
-                ':ne': {'S': nombre_error},
-                ':te': {'S': str(diferencia)},
-                ':tgl':{'S':fecha_t_g_l},
-            }
+            UpdateExpression = l_UpdateExpression,
+            ExpressionAttributeValues = l_Atributos
         )
